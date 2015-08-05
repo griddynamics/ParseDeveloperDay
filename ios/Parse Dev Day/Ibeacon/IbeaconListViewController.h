74f91d12a5f585f9af13db9d1a212df99d9cef09
abeliakov <abelyakov@griddynamics.com>
2015-08-05 19:33:02 +0300

    new tab

---
+++ ios/Parse Dev Day/Ibeacon/IbeaconListViewController.h
@@ -0,0 +1,12 @@
+//
+//  IbeaconListViewController.h
+//  Parse Dev Day
+//
+//  Created by Andrey Belyakov on 8/5/15.
+//  Copyright (c) 2015 Parse. All rights reserved.
+//
+
+#import "PDDBaseListViewController.h"
+
+@interface IbeaconListViewController : PDDBaseListViewController
+@end
